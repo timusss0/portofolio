3563a17865eedc067a25aee3816586e7c15a489a
timusss0 <mustikavia86@gmail.com>
2023-08-02 20:17:29 +0700

    sertifikat

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
       letterSpacing: {
         sempit:'-0.5em',
         lebarbet: '.7em',
-      }
+      },
+      width: {
+        128: '32rem',
+      },
     },
   },
   plugins: [],
